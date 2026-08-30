183233f27569c0fa94c9f034177afe4b6cf92264
Angela Lee <angela.tylee25@gmail.com>
2026-08-30 17:41:11 +0800

    實作：調整表頭資訊，設置回傳 JSON 與 cors 資訊

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const http = require('http');
 const requestListener = (req, res) => {
   console.log(req.method, req.url);
   const header = {
-    "Content-Type": "text/plain"
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
   };
   if (req.url == '/') {
     res.writeHead(200, header);
-    res.write("index");
+    res.write(JSON.stringify({
+      "status": "success",
+      "data": [],
+    }));
     res.end();
   } else {
     res.writeHead(404, header);
-    res.write("此頁面不存在");
+    res.write(JSON.stringify({
+      "status": "false",
+      "message": "無此網站路由",
+    }));
     res.end();
   }
 }
